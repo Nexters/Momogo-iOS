194a3e88437f5e4b9a069c84e0953773284b6383
gomhyeok <gomhyeok@gmail.com>
2026-07-28 00:33:33 +0900

    #Momogo/005 : Gray400/200 컬러값 Figma 실제 값으로 수정

--- Projects/Shared/DesignSystem/Sources/Foundation/Color/Color+Gray.swift
+++ Projects/Shared/DesignSystem/Sources/Foundation/Color/Color+Gray.swift
@@ -7,9 +7,9 @@ public extension DesignSystem.Color {
     static let gray700 = SwiftUI.Color(momogoHex: 0x464443)
     static let gray600 = SwiftUI.Color(momogoHex: 0x5A5756)
     static let gray500 = SwiftUI.Color(momogoHex: 0x63605F)
-    static let gray400 = SwiftUI.Color(momogoHex: 0xB9C1C9)
+    static let gray400 = SwiftUI.Color(momogoHex: 0x82807F)
     static let gray300 = SwiftUI.Color(momogoHex: 0x969494)
-    static let gray200 = SwiftUI.Color(momogoHex: 0xEBEFF5)
+    static let gray200 = SwiftUI.Color(momogoHex: 0xB7B6B5)
     static let gray100 = SwiftUI.Color(momogoHex: 0xCFCECD)
     static let gray50 = SwiftUI.Color(momogoHex: 0xEFEFEF)
 }
